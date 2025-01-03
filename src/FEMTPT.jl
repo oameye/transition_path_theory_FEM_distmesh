e7cca039deb57d01078848ce3cc99cbb85f65dfa
Orjan Ameye <orjan.ameye@hotmail.com>
2025-01-03 16:11:33 +0100

    move python stuff

--- src/FEMTPT.jl
+++ src/FEMTPT.jl
@@ -14,6 +14,7 @@ include("tpt.jl")
 export distmesh2D, dellipse, ddiff
 export put_pts_on_circle, put_pts_on_ellipse, reparametrization
 export find_ABbdry_pts_ellipse
+export  FEM_committor_solver_Langevin, invariant_pdf, reactive_current_transition_rate_Langevin, probability_reactive_Langevin, probability_last_A_Langevin
 
 
 end
